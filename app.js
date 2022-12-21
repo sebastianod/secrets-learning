f5d208f9e4132b91b0b1bfd5bb77736e176cc529
sebastian <sebastianochoad@gmail.com>
2022-12-20 21:20:48 -0500

    level 4: Salting and hashing with bcrypt hash

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -42,34 +43,39 @@ app.get("/register", function (req, res) {
 //------------Post methods--------------//
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username, //username and password is their name in register.ejs
-    password: md5(req.body.password) //save the hashed password
-  });
-
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets"); //if registered, take to secrets page
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //take the written password and run a bcrypt hash, hash is the result
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username, //username and password is their name in register.ejs
+      password: hash, //save the hashed password as the user password
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets"); //if registered, take to secrets page
+      }
+    });
   });
 });
 
 app.post("/login", function (req, res) {
   const enteredEmail = req.body.username; //entered email and password
-  const enteredPass = md5(req.body.password); //md5# always produces the same key
+  const enteredPass = req.body.password; //md5# always produces the same key
 
   User.findOne({ email: enteredEmail }, function (err, foundUser) {
     if (!err) {
       if (foundUser) {
-        //if a user with the entered email is found, then...
-        if (foundUser.password === enteredPass) {
-          //if its password is right, then...
-          res.render("secrets");
-        } else {
-          res.send("Check if your email and password match.");
-        }
+        bcrypt.compare(enteredPass, foundUser.password, function (err, result) {
+          //compares enteredPassword with the hashed version during registry = foundUser.password
+          // result == true or false
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.send("Check your password and email combination!");
+          }
+        });
       } else {
         //if no user is found
         res.send("That user doesn't exist! check your email.");
