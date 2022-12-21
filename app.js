044a3e8bcd25bb5c62eddd25b006ff06dbbd9f10
sebastian <sebastianochoad@gmail.com>
2022-12-20 20:36:54 -0500

    Level 3 encryption: Hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,13 +22,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-//-------------Encryption---------------//
-
-//Adding the Encryption plug-in to the schema. Must be before setting the model.
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
-//process.env.SECRET grabs the SECRET key from the .env file.
-
-//--------------------------------------//
 
 const User = new mongoose.model("User", userSchema);
 
@@ -51,7 +44,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username, //username and password is their name in register.ejs
-    password: req.body.password,
+    password: md5(req.body.password) //save the hashed password
   });
 
   newUser.save(function (err) {
@@ -65,7 +58,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const enteredEmail = req.body.username; //entered email and password
-  const enteredPass = req.body.password;
+  const enteredPass = md5(req.body.password); //md5# always produces the same key
 
   User.findOne({ email: enteredEmail }, function (err, foundUser) {
     if (!err) {
